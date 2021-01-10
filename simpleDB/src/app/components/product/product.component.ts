349639708172621f95e7817f9f36028df2ca6aec
gates17 <alexandre.lecom@gmail.com>
2021-01-10 00:54:52 +0000

    product field mapping fix

--- simpleDB/src/app/components/product/product.component.ts
+++ simpleDB/src/app/components/product/product.component.ts
@@ -1,9 +1,10 @@
-import { element } from 'protractor';
+import { ProducttypeService } from './../../services/producttype.service';
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormControl, FormGroup } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 import jspdf from 'jspdf';
 import 'jspdf-autotable';
@@ -37,6 +38,7 @@ export class ProductComponent implements OnInit {
   })
   constructor(
     private productService: ProductService,
+    private typeService: ProducttypeService,
     private router: Router,
     private location: Location
   ) { }
@@ -59,16 +61,42 @@ export class ProductComponent implements OnInit {
   }
 
   readProduct(): void {
+
     this.productService.getAll(this.access_token)
       .subscribe(
-        product => {
-          this.product = product;
+        product =>
+        {
+          for(let p of product){
+            this.typeService.get(p.type_id).subscribe(result => { p.type_id=result[0].description})
+          }
+          this.product = product
         },
         error => {
 
           this.gotoLogin();
         });
+/*
+    this.productService.getAll(this.access_token).pipe(map(r=> {
+      for(let p of r){
+        p.type_id = this.typeService.get(p.type_id).subscribe(result => {})
+      }
+      console.log(r)
+      this.product = r
+    }))
+    .subscribe(
+      product =>
+      {
+        this.product = product
+        console.log(this.product)
+      },
+      error => {
+
+        this.gotoLogin();
+      });
+*/
   }
+
+
   softDelete(id){
     this.productService.get(id).subscribe(result => {
       this.pDel = result[0]
@@ -88,21 +116,22 @@ export class ProductComponent implements OnInit {
     let prod: any;
     let searchresults: any = [];
     let searchParam = event.target.value;
-    console.log(this.product)
-    console.log(searchParam)
     if(searchParam === "") { this.searchresults = []}
     if(searchParam !== "" && searchParam !==null && searchParam !== undefined)
     {
       for(prod of this.product){
+        let match = false
         for(let key in prod) {
           let value = prod[key]
-            if (value && value.toString() && value.toString().includes(searchParam.toString())) {
-              searchresults.push(prod)
+            if (value && value.toString() && value.toString().toLowerCase().includes(searchParam.toString().toLowerCase())) {
+              match=true
             }
+        }
+        if(match===true) {
+          searchresults.push(prod)
         }
       }
       this.searchresults=searchresults
-      console.log(searchresults)
     }
   }
 
